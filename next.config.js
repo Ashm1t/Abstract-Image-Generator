1bb88f835d5d9bac60fa3e71f070d701d0f272db
ashm1t <ashmit44944@gmail.com>
2025-06-08 14:28:17 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/Abstract-Image-Generator',
+  env: {
+    BACKEND_URL: 'https://abstract-image-generator-production.up.railway.app'
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': './src'
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig 
